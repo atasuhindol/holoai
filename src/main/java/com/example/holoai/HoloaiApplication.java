8f6a1b99e318e9fa243b348a79cdecae886292d3
ata suhindol <suhindolata@gmail.com>
2025-05-28 05:59:00 +0300

    Initial commit

---
+++ src/main/java/com/example/holoai/HoloaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.holoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoloaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoloaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holoai/HoloaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoloaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
